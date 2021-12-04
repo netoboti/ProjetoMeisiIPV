99cd7ca18521be3b2d27d47af270b608b2671a63
Oswaldo B. Neto <netoboti@gmail.com>
2021-12-04 10:15:36 +0000

    Geral

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter_windows_2.5.3-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\EI_Trabalhos\ProjetoMeisiIPV"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\OswaldoNeto\Documents\GitHub\ProjetoMeisiIPV"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:uber/telas/Home.dart';
-import 'Rotas.dart';
 
-final ThemeData temaPadrao = ThemeData(
-    primaryColor: Color(0xff37474f),
-    accentColor: Color(0xff546e7a),
-    appBarTheme: AppBarTheme(
-        backgroundColor: Color(0xffe5c100),
-        foregroundColor: Color(0xffffffff)
-                            )
-);
+import 'home_page.dart';
 
-void main() => runApp(MaterialApp(
-  title: "ParkingM",
-  home: Home(),
-  theme: temaPadrao,
-  initialRoute: "/",
-  onGenerateRoute: Rotas.gerarRotas,
-  debugShowCheckedModeBanner: false,
-));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ParkingM IPV',
+      home: HomePage(),
+    );
+  }
+}
